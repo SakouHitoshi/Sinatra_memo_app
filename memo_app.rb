024a317ac07525ed82ade749f5229ef267802bb9
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-02-28 11:36:54 +0900

    app-create

--- memo_app.rb
+++ memo_app.rb
@@ -1,23 +1,60 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'erb'
+require 'json'
 
-#  ホーム
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+not_found do
+  'ファイルが存在しません'
+end
+
 get '/' do
+  redirect '/memos'
+end
+
+get "/memos" do
+  @title = "メモ一覧"
+  files = Dir.glob("memos/*").sort_by { |file| File.mtime(file) }
+  @memos = files.map { |file| JSON.parse(File.read(file), symbolize_names: true)}
   erb :home
 end
 
-# 投稿
-get '/new' do
+get '/memos/new' do
+  @title = "メモ作成"
   erb :new
 end
 
-# 詳細
-get '/show' do
+post '/memos/new' do
+  hash = { id: SecureRandom.uuid, title: params[:title], content: params[:content] }
+  File.open("memos/#{hash[:id]}.json", 'w') { |file| file.puts JSON.generate(hash) }
+  redirect '/memos'
+end
+
+get '/memos/:id' do
+  @title = 'メモ詳細'
+  @memo = JSON.parse(File.read("memos/#{params[:id]}.json"), symbolize_names: true)
   erb :show
 end
 
-# 変更
-get '/edit' do
+get '/memos/:id/edit' do
+  @title = 'メモ内容変更'
+  @memo = JSON.parse(File.read("memos/#{params[:id]}.json"), symbolize_names: true)
   erb :edit
+end
+
+patch "/memos/:id" do
+  File.open("memos/#{params[:id]}.json", 'w') do |file|
+    hash = { id: params[:id], title: params[:title], content: params[:content] }
+    JSON.dump(hash, file)
+  end
+  redirect "/memos/#{params[:id]}"
+end
+
+delete '/memos/:id' do
+  File.delete("memos/#{params[:id]}.json")
+  redirect '/memos'
 end
